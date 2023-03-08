fe1fd94acede3c49d18041de2cdc8a2aa7f63f65
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-03-08 03:45:05 +0300

    Task cards created and  listed on main page

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function TaskShow({ task }) {
   console.log(task);
-  return <div></div>;
+  return (
+    <div className="task-show">
+      <h3 className="task-title"> Task Title </h3>
+      <p>{task.title}</p>
+      <h3 className="task-title"> Task Description </h3>
+      <p> {task.description} </p>
+      <div>
+        <button className="task-delete">
+          <b>Delete</b>
+        </button>
+        <button className="task-edit">
+          <b>Update</b>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TaskShow;
