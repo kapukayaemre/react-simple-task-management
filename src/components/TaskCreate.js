48148a87e176763b5c401f097e0d2ff3c245c7a1
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-03-08 04:21:21 +0300

    Update page integrated

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-function TaskCreate({ onCreate }) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+function TaskCreate({ onCreate, task, taskFormUpdate }) {
+  const [title, setTitle] = useState(task ? task.title : "");
+  const [description, setDescription] = useState(task ? task.description : "");
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -21,26 +21,56 @@ function TaskCreate({ onCreate }) {
   };
 
   return (
-    <div className="task-create">
-      <h3>Add New Task!</h3>
-      <form className="task-form">
-        <label className="task-label">Task Title</label>
-        <input
-          value={title}
-          onChange={handleTitleChange}
-          className="task-input"
-        />
-        <label className="task-label">Task Content</label>
-        <textarea
-          value={description}
-          onChange={handleDescriptionChange}
-          className="task-input"
-          rows={5}
-        />
-        <button className="task-button" onClick={handleSubmit}>
-          Set Task
-        </button>
-      </form>
+    <div>
+      {" "}
+      {taskFormUpdate ? (
+        <div className="task-update">
+          <h3>Update This Task!</h3>
+          <form className="task-form">
+            <label className="task-label">Current Task Title</label>
+            <input
+              value={title}
+              onChange={handleTitleChange}
+              className="task-input"
+            />
+            <label className="task-label">Current Task Content</label>
+            <textarea
+              value={description}
+              onChange={handleDescriptionChange}
+              className="task-input"
+              rows={5}
+            />
+            <button
+              className="task-button update-button"
+              onClick={handleSubmit}
+            >
+              Update
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="task-create">
+          <h3>Add New Task!</h3>
+          <form className="task-form">
+            <label className="task-label">Task Title</label>
+            <input
+              value={title}
+              onChange={handleTitleChange}
+              className="task-input"
+            />
+            <label className="task-label">Task Content</label>
+            <textarea
+              value={description}
+              onChange={handleDescriptionChange}
+              className="task-input"
+              rows={5}
+            />
+            <button className="task-button" onClick={handleSubmit}>
+              Set Task
+            </button>
+          </form>
+        </div>
+      )}{" "}
     </div>
   );
 }
--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,26 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import TaskCreate from "./TaskCreate";
 
 function TaskShow({ task, onDelete }) {
-  
-    const handleDeleteClick = () => {
-        onDelete(task.id)
-    }
+  const [showEdit, setShowEdit] = useState(false);
 
+  const handleDeleteClick = () => {
+    onDelete(task.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
 
   return (
     <div className="task-show">
-      <h3 className="task-title"> Task Title </h3>
-      <p>{task.title}</p>
-      <h3 className="task-title"> Task Description </h3>
-      <p> {task.description} </p>
-      <div>
-        <button className="task-delete" onClick={handleDeleteClick}>
-          <b>Delete</b>
-        </button>
-        <button className="task-edit">
-          <b>Update</b>
-        </button>
-      </div>
+      {showEdit ? (
+        <TaskCreate task={task} taskFormUpdate={true} />
+      ) : (
+        <div>
+          <h3 className="task-title"> Task Title </h3>
+          <p>{task.title}</p>
+          <h3 className="task-title"> Task Description </h3>
+          <p> {task.description} </p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              <b>Delete</b>
+            </button>
+            <button className="task-edit" onClick={handleEditClick}>
+              <b>Edit</b>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
